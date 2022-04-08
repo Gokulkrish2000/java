90cb5965f774c3d3ff6c74b52c8ebbf7a25df389
Gokulkrish2000 <103176665+Gokulkrish2000@users.noreply.github.com>
2022-04-08 13:49:01 +0530

    Add files via upload

---
+++ practicals/arrayExample.java
@@ -0,0 +1,13 @@
+class arrayExample{
+public static void main(String args[]){
+String [] names ={"gokul","ganesh","monish","sathish","poojasri","nandhini"};
+for (int i=0;i<names.length;i++){
+System.out.println(names[i]);
+}
+System.out.println();
+int [] phones ={123,345,567,789,901};
+for (int i=0;i<phones.length;i++){
+System.out.println(phones[i]);
+}
+}
+}
